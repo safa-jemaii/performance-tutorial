d2da0ba8849e5029ed5ee6c6f82d974d39be0957
safa-jemaii <safajemaii48@gmail.com>
2022-12-15 14:20:53 +0100

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,39 @@
+import React, { useCallback,useReducer, useState } from 'react';
+import './App.css';
+import CharacterMap from '../CharacterMap/CharacterMap';
+
+
+function App() {
+  const [text, setText] = useState('');
+  const [showExplanation, toggleExplanation] = useReducer(state => !state, false)
+  const transformer = useCallback(item => item.toLowerCase(), []);
+
+
+  return(
+    <div className="wrapper">
+      <label htmlFor="text">
+        <p>Your Text</p>
+        <textarea
+          id="text"
+          name="text"
+          rows="10"
+          cols="100"
+          onChange={event => setText(event.target.value)}
+        >
+        </textarea>
+      </label>
+      <CharacterMap text={text} />
+      <div>
+        <button onClick={toggleExplanation}>Show Explanation</button>
+      </div>
+      {showExplanation &&
+        <p>
+          This displays a list of the most common characters.
+        </p>
+      }
+      <CharacterMap showExplanation={showExplanation} text={text} transformer={transformer} />
+    </div>
+  )
+}
+
+export default App;
